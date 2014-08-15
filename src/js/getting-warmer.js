bf40d48270875dc96e1798325a4555827805eea0
Okal Otieno <okal@justokal.com>
2014-08-15 15:43:41 +0300

    Add paint logic

--- src/js/getting-warmer.js
+++ src/js/getting-warmer.js
@@ -2,26 +2,58 @@ var jsSHA = require('jssha');
 var _ = require('underscore');
 
 function GettingWarmer(setup) {
-	var paint, canvas, generateFillColour;
+	var paint, canvas, context, generateFillColour;
 	var passwordInput = document.querySelector(setup.passwordInputSelector);
 	var thumbnailContainer = document.querySelector(setup.thumbnailContainerSelector);
 
 	var init = function() {
 		canvas = document.createElement('canvas');
-		canvas.setAttribute('height', '64');
-		canvas.setAttribute('width', '64');
+		context = canvas.getContext('2d');
+		canvas.setAttribute('height', '256');
+		canvas.setAttribute('width', '256');
 		thumbnailContainer.appendChild(canvas);
-		passwordInput.onkeyup = paint
+		passwordInput.onkeyup = paint;
+		paint();
 	}
 
 	generateFillColour = function(string) {
-		
+		var color = '#', hash = 0;
+		for(var i = 0; i < string.length; i++) {
+			hash = string.charCodeAt(i) + ((hash << 5) - hash);
+		}
+		for(var i = 0; i < 3; i++) {
+			var value = (hash >> (i * 8)) & 0xFF;
+			color += ('00' + value.toString(16)).substr(-2);
+		}
+		return color;
 	}
 
 	paint = function() {
+		canvas.width = canvas.width
 		var seed = new jsSHA((this.value), 'TEXT').getHash('SHA-256', 'HEX');
-		var instructions = seed.match(/.{1,4}/g);
-		Array.map(instructions);
+		var instructions = seed.match(/.{1,1}/g);
+		var color;
+		_.each(instructions, function(instruction, index) {
+			var x, y;
+			x = ((index + 1) % 2) ? (256 - (index * 8.3)) : (index * 8.2);
+			y = (Math.log(x) % 1) * 256;
+			var length = index;
+			var alpha = Math.log((index + 1) / (index % 16)) % 1;
+			var color = generateFillColour(instruction + index);
+			context.globalAlpha = 1 - alpha;
+			context.fillStyle = color;
+			context.rotate((index + 1) * 360 / 16);
+			console.log(x)
+			console.log(y)
+			context.fillRect(3, 3, x, y);
+			context.beginPath();
+			context.bezierCurveTo(x + (Math.log(parseInt(seed)) % 1) * 256, y + (Math.log(parseInt(seed))), x + 32, y - 67, x, Math.abs(6 - y));
+			context.bezierCurveTo(x + 32, y - 67, x, x + (Math.log(parseInt(seed)) % 1) * 256, y + (Math.log(parseFloat(instruction))), Math.abs(6 - y));
+			context.lineWidth = (Math.log(seed) % 1) * 32;
+			context.strokeStyle = generateFillColour(seed + seed);
+			context.stroke();
+			context.rotate((index + 1) * 360 / Math.log(parseInt(seed)));
+		})
 		console.log(seed);
 	}
 
